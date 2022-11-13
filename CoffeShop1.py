56a5a096a382bd40d9662cd00ce341e836053ccf
lanmeglc <70088226+lanmeglc@users.noreply.github.com>
2022-11-13 11:53:34 +0100

    Add files via upload

---
+++ CoffeShop1.py
@@ -0,0 +1,19 @@
+print("Hello, welcome to Lan's Coffe shop!!!!!!!!")
+
+name = input("What is your name?\n")
+
+print("Hello " + name + " thank you so much for coming in tonight.")
+
+menu = "Menu: coffe and sandwich or coffe and eggs on our menu"
+
+print(name, "here is your menu. \n" + menu)
+
+order = input("What do you want " + name + "? ")
+
+print("Your " + order + " will be ready in 10 minutes, Thank you very much for your order. ")
+
+
+
+
+
+
